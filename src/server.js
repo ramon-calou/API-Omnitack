ba8ba66050f4650170d529b9990447ee79985ba8
ramoncalou <ramon.alves@al.infnet.edu.br>
2019-10-09 14:46:13 -0300

    API Finalizada

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect('mongodb+srv://nodeUser:9306@nodeapi-c8uq6.mongodb.net/stack?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+//RealTime Middleware
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3001);
